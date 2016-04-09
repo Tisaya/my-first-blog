a07e618845f97eb04dc4588ee1aa58ff390c0f6d
Joanna <joanna.ufnowska@gmail.com>
2016-04-09 16:58:01 +0200

    Dodano widok i szablon dla poszczegolnego wpisu na stronie

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone   # importujemy dla funkcji timezone.now()
 from .models import Post        # dołaczamy model napisany w models.py
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -9,3 +10,7 @@ from .models import Post        # dołaczamy model napisany w models.py
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  # tworzymy zmienna posts dla QuerySetu
     return render (request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
